dbc8d22bef4e2ffea9e55104c54db1482f9fb53a
dungeonaltaf <altafsofficial01@gmail.com>
2021-01-15 20:31:52 +0530

    just server side

--- src/app/posts/comment.model.ts
+++
@@ -1,5 +0,0 @@
-export interface Comment {
-  id: string;
-  user_id: string;
-  content: string;
-}
--- src/app/posts/post.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Post {
-  id: string;
-  title: string;
-  content: string;
-  price: number;
-  comments: Array<{comment:string,commentator:string}>;
-}

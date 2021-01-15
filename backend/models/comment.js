746cc91c7479858a1036946f24c59ec681537a33
dungeonaltaf <altafsofficial01@gmail.com>
2021-01-15 21:38:34 +0530

    prod created

--- backend/models/comment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const commentSchema = mongoose.Schema({
-  post_id: {type: String, required: true},
-  user_id: {type: String, required: true},
-  content: {type: String, required: true}
-});
-
-module.exports = mongoose.model('Comment',commentSchema)
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "http://marketplace-env.eba-rcnb8bcb.ap-south-1.elasticbeanstalk.com/"
 };
